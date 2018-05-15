e656d16e8f85b863336c4ed148c73e24ff18f921
tmdals820 <git@github.com:tmdals820/Team-A.git>
2018-05-15 19:34:32 +0900

    Add LoginInfoService service annotation and spring context file

--- ims/src/ims/sunmmon/service/login/LoginServiceImpl.java
+++ ims/src/ims/sunmmon/service/login/LoginServiceImpl.java
@@ -1,11 +1,18 @@
 package ims.sunmmon.service.login;
 
+import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 
+import org.springframework.stereotype.Service;
+
 import ims.sunmmon.domain.LoginInfo;
+import ims.sunmmon.persistance.LoginInfoMapper;
 
+@Service
 public class LoginServiceImpl implements LoginService {
-
+	@Resource
+	private LoginInfoMapper loginInfoMapper;
+	
 	@Override
 	public LoginInfo login(HttpServletRequest request) throws Exception {
 		return null;
