77ae495ac7f353377b13fb679b479477e4aa37ef
tmdals820 <git@github.com:tmdals820/Team-A.git>
2018-05-17 22:48:36 +0900

    Fix Mapper Interface

--- ims/src/ims/sunmmon/persistance/LoginInfoMapper.java
+++ ims/src/ims/sunmmon/persistance/LoginInfoMapper.java
@@ -1,17 +1,9 @@
 package ims.sunmmon.persistance;
 
-import java.util.List;
-
 import ims.sunmmon.domain.LoginInfo;
 
 public interface LoginInfoMapper {
-	public List<LoginInfo> list(LoginInfo loginInfo);
-
-	public void insert(LoginInfo loginInfo);
-
 	public LoginInfo select(LoginInfo loginInfo);
 
 	public void update(LoginInfo loginInfo);
-
-	public void delete(String id);
 }
