78c2ad7144aff1345f86e37b7103be705f497e86
tmdals820 <git@github.com:tmdals820/Team-A.git>
2018-05-23 01:35:54 +0900

    Add query using join

--- ims/src/ims/sunmoon/persistance/BDMapper.java
+++ ims/src/ims/sunmoon/persistance/BDMapper.java
@@ -7,6 +7,10 @@ import ims.sunmoon.domain.BD;
 public interface BDMapper {
 	public List<BD> list(BD bd);
 
+	public List<BD> findItem(BD bd);
+
+	public List<BD> findClient(BD bd);
+
 	public void insert(BD bd);
 
 	public BD select(BD bd);
--- ims/src/ims/sunmoon/service/bd/BDServiceImpl.java
+++ ims/src/ims/sunmoon/service/bd/BDServiceImpl.java
@@ -8,7 +8,6 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ims.sunmoon.util.option.sort.BDSortOption;
 import ims.sunmoon.domain.BD;
 import ims.sunmoon.persistance.BDMapper;
 
@@ -19,7 +18,40 @@ public class BDServiceImpl implements BDService {
 
 	@Override
 	public List<BD> list(BD bd) {
+		bd.setUseable(1);
 		return this.bdMapper.list(bd);
+	}
+
+	@Override
+	public List<BD> list(BD bd, String keyword) {
+		List<BD> find = null;
+		bd.setKeyword(keyword);
+		if (bd.getFindOption() != null) {
+			switch (bd.getFindOption()) {
+			case ITEM_CODE:
+			case ITEM_NAME:
+				find = this.bdMapper.findItem(bd);
+				break;
+
+			case CLIENT_NAME:
+				find = this.bdMapper.findClient(bd);
+				break;
+
+			case CON_VER:
+				bd.setConVer(keyword);
+				find = this.bdMapper.list(bd);
+				break;
+			}
+		}
+		return find;
+	}
+
+	@Override
+	public List<BD> list(Date first, Date last) {
+		BD find = new BD();
+		find.setFirst(first);
+		find.setLast(last);
+		return this.list(find);
 	}
 
 	@Override
@@ -31,6 +63,13 @@ public class BDServiceImpl implements BDService {
 	@Override
 	public BD view(BD bd) {
 		return this.bdMapper.select(bd);
+	}
+
+	@Override
+	public BD view(String bdNo) {
+		BD find = new BD();
+		find.setBdNo(Integer.parseInt(bdNo));
+		return this.view(find);
 	}
 
 	@Override
