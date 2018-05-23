599b525df279e650e1d61f2c37432d386f56e784
tmdals820 <git@github.com:tmdals820/Team-A.git>
2018-05-23 13:10:40 +0900

    Refactoring code

--- ims/src/ims/sunmoon/service/bereleased/BereleasedServiceImpl.java
+++ ims/src/ims/sunmoon/service/bereleased/BereleasedServiceImpl.java
@@ -7,7 +7,6 @@ import javax.annotation.Resource;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import ims.sunmoon.util.option.sort.BereleasedSortOption;
 import ims.sunmoon.domain.Bereleased;
 import ims.sunmoon.persistance.BereleasedMapper;
 
