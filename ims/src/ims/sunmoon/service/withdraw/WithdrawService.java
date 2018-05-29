5d872d571ca97033d1f7b0c614f83a23df9ff76d
tmdals820 <git@github.com:tmdals820/Team-A.git>
2018-05-29 20:57:49 +0900

    TODO: 입/출금 및 매입/매출 및 발주/수주 보고서 조회 구현, Withdraw 컨트롤러 로직 구현하기

--- ims/src/ims/sunmoon/service/withdraw/WithdrawService.java
+++ ims/src/ims/sunmoon/service/withdraw/WithdrawService.java
@@ -1,17 +1,24 @@
 package ims.sunmoon.service.withdraw;
 
+import java.sql.Date;
 import java.util.List;
 
 import ims.sunmoon.domain.Withdraw;
 
 public interface WithdrawService {
 	public List<Withdraw> list(Withdraw withdraw);
-	
+
+	public List<Withdraw> list(Date first, Date last);
+
+	public List<Withdraw> list(Withdraw withdraw, String keyword);
+
 	public void add(Withdraw withdraw);
 
 	public Withdraw view(Withdraw withdraw);
+	
+	public Withdraw view(String withNo);
 
 	public void edit(Withdraw withdraw);
 
-	public void remove(String withdrawNo);
+	public void remove(String withNo);
 }
--- ims/src/ims/sunmoon/service/withdraw/WithdrawServiceImpl.java
+++ ims/src/ims/sunmoon/service/withdraw/WithdrawServiceImpl.java
@@ -1,5 +1,6 @@
 package ims.sunmoon.service.withdraw;
 
+import java.sql.Date;
 import java.util.List;
 
 import javax.annotation.Resource;
@@ -17,6 +18,8 @@ public class WithdrawServiceImpl implements WithdrawService {
 
 	@Override
 	public List<Withdraw> list(Withdraw withdraw) {
+		withdraw.setUseable(1);
+
 		return this.withdrawMapper.list(withdraw);
 	}
 
@@ -28,6 +31,8 @@ public class WithdrawServiceImpl implements WithdrawService {
 
 	@Override
 	public Withdraw view(Withdraw withdraw) {
+		withdraw.setUseable(1);
+
 		return this.withdrawMapper.select(withdraw);
 	}
 
@@ -41,5 +46,57 @@ public class WithdrawServiceImpl implements WithdrawService {
 	@Transactional
 	public void remove(String withdrawNo) {
 		this.withdrawMapper.delete(withdrawNo);
+	}
+
+	@Override
+	public List<Withdraw> list(Date first, Date last) {
+		Withdraw find = new Withdraw();
+		find.setFirst(first);
+		find.setLast(last);
+
+		return this.list(find);
+	}
+
+	@Override
+	public List<Withdraw> list(Withdraw withdraw, String keyword) {
+		List<Withdraw> find = null;
+		withdraw.setKeyword(keyword);
+
+		if (withdraw.getFindOption() != null) {
+			switch (withdraw.getFindOption()) {
+			case NONE:
+				find = this.list(withdraw);
+				break;
+
+			case WITH_NO:
+				withdraw.setWithNo(Integer.parseInt(keyword));
+				find = this.list(withdraw);
+				break;
+
+			case CLEINT_NO:
+				withdraw.setClientNo(Integer.parseInt(keyword));
+				find = this.list(withdraw);
+				break;
+
+			case CLIENT_NAME:
+				find = this.withdrawMapper.find(withdraw);
+				break;
+
+			case CON_VER:
+				withdraw.setConVer(keyword);
+				find = this.list(withdraw);
+				break;
+			}
+		}
+
+		return find;
+	}
+
+	@Override
+	public Withdraw view(String withNo) {
+		Withdraw find = new Withdraw();
+		find.setWithNo(Integer.parseInt(withNo));
+
+		return this.view(find);
 	}
 }
