0549ab4168f8b3c0faf8c04eb7046ef1fefa5994
tmdals820 <git@github.com:tmdals820/Team-A.git>
2018-05-30 01:48:25 +0900

    Implements Login & Logout Logic

--- ims/src/ims/sunmoon/interceptor/LoginInfoInterceptor.java
+++ ims/src/ims/sunmoon/interceptor/LoginInfoInterceptor.java
@@ -2,6 +2,7 @@ package ims.sunmoon.interceptor;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -9,6 +10,24 @@ public class LoginInfoInterceptor extends HandlerInterceptorAdapter {
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
+		HttpSession session = request.getSession(true);
+
+		Boolean isLogin = session.getAttribute("isLogin") != null ? (Boolean) session.getAttribute("isLogin") : false;
+
+		if (!isLogin) {
+			response.sendRedirect(request.getContextPath() + "/login");
+			return false;
+		} else {
+			String url = request.getRequestURL().toString();
+			String path = url.substring(url.lastIndexOf("/"), url.length());
+
+			if ("".equals(path) || path == null || "/".equals(path)) {
+				session.invalidate();
+				response.sendRedirect(request.getContextPath() + "/login");
+				return false;
+			}
+		}
+
 		return super.preHandle(request, response, handler);
 	}
 }
